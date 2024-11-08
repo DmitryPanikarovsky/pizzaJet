f0c4f2d3061479f844273dc8d9daef19228f4ba3
DmitryPanikarovsky <dmitry.panikarovsky@yandex.ru>
2024-11-08 10:51:03 +0300

    create state-redux for pagination

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,10 @@ import styles from "./Home.module.scss";
 
 import { useContext, useEffect, useState } from "react";
 import { SearchContext } from "../../App";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+
+import { setPageCount } from "../../redux/slices/filterSlice";
 
 import { Sorting } from "../../component/Sorting/Sorting";
 import { Categories } from "../../component/Categories/Categories";
@@ -11,16 +13,18 @@ import { Skeleton } from "../../component/PizzaBlock/Skeleton";
 import { PizzaBlock } from "../../component/PizzaBlock/PizzaBlock";
 import { Pagination } from "../../component/Pagination/Pagination";
 
-
 export const Home = () => {
-    const { categoryId, sortType } = useSelector((state) => state.filterReducer);  
+    const { categoryId, sortType, pageCount } = useSelector((state) => state.filterReducer);
+    const dispatch = useDispatch();
 
     const { searchValue } = useContext(SearchContext);
     const [pizzas, setPizzas] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [open, setOpen] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
 
+    const onChangePage = (number) => {
+        dispatch(setPageCount(number));
+    };
 
     useEffect(() => {
         const sortBy = sortType.properties.replace("-", "");
@@ -28,7 +32,7 @@ export const Home = () => {
         const category = categoryId > 0 ? `category=${categoryId}` : "";
         const search = searchValue ? `&search=${searchValue}` : "";
 
-        const data = `?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`;
+        const data = `?page=${pageCount}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`;
 
         setIsLoading(true);
         axios.get(`https://66f2d5e071c84d805876ef77.mockapi.io/pizzas${data}`).then((response) => {
@@ -36,8 +40,7 @@ export const Home = () => {
             setIsLoading(false);
         });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType.properties, searchValue, currentPage]);
-
+    }, [categoryId, sortType.properties, searchValue, pageCount]);
 
     return (
         <div className={styles.Home} onClick={() => setOpen(false)}>
@@ -52,7 +55,7 @@ export const Home = () => {
                         ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
                         : pizzas.map((item) => <PizzaBlock key={item.id} {...item} />)}
                 </div>
-                <Pagination onPageChange={(number) => setCurrentPage(number)} />
+                <Pagination onPageChange={onChangePage} />
             </div>
         </div>
     );
